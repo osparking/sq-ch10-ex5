9cd48982a29005013523b6d46110c9576dbd013e
osparking <jbpark03@gmail.com>
2023-10-17 15:57:21 +0900

    initial for exception handling

---
+++ src/main/java/sq_ch10_ex5/SqCh10Ex5Application.java
@@ -0,0 +1,13 @@
+package sq_ch10_ex5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqCh10Ex5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqCh10Ex5Application.class, args);
+	}
+
+}
---
+++ src/test/java/sq_ch10_ex5/SqCh10Ex5ApplicationTests.java
@@ -0,0 +1,13 @@
+package sq_ch10_ex5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqCh10Ex5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
